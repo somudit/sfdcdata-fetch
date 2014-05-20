966f8ea9b216bb4d4ef1edf08fb628efe6cb1389
mudits1990 <mudits1990@gmail.com>
2014-05-20 11:42:36 +0530

    Class and methods

--- sfdc.py
+++ sfdc.py
@@ -1,19 +1,34 @@
-from simple_salesforce import Salesforce
-import json
-from collections import OrderedDict
+from simple_salesforce import Salesforce, SalesforceLogin, SFType
 
 class sfdcdatafetch():
     def __init__(self, username, password, security_token):
-        sf = Salesforce(username=username, password=password, security_token=security_token)
-        query = "Select "
-        for x in sf.Campaign__c.describe()['fields']:
-          query = query + x['name'] + ","
-        query = query[:-1] + " from Campaign__c"
-        print query
-        res = sf.query(query)
-        records = res['records']
-        for x in records:
-            for y in sf.Campaign__c.describe()['fields']:
-                print x[y['name']]
+        self.username = username
+        self.password = password
+        self.security_token = security_token
 
-data = sfdcdatafetch('mudit.somani@salesforce.com','mudit1234$','N0OF2max8RxHHoZkY4AB96uih')
+    def returnsObject(self):
+        sf = Salesforce(username=self.username, password=self.password, security_token=self.security_token)
+        l1 = [[x['name'],x['custom'],x['label']] for x in sf.describe()['sobjects']]
+        return l1
+    def runQuery(self,*args):
+        sf = Salesforce(username=self.username, password=self.password, security_token=self.security_token)
+        session_id = sf.session_id
+        instance = sf.sf_instance
+        print session_id, instance
+        query = "Select "
+        #session_id, instance = SalesforceLogin(self.username, self.password, self.security_token, True)
+        for sObject in args:
+            sObjectName = SFType(sObject[0],session_id,instance)
+            for x in sObjectName.describe()['fields']:
+                query = query + x['name'] + ","
+            query = query[:-1] + " from " + sObjectName.name
+            print query
+            res = sf.query(query)
+            records = res['records']
+            for x in records:
+                for y in sObjectName.describe()['fields']:
+                    print x[y['name']]
+
+sfdc = sfdcdatafetch('mudit.somani@salesforce.com','mudit1234$','N0OF2max8RxHHoZkY4AB96uih')
+print sfdc.returnsObject()
+sfdc.runQuery(['Campaign__c',False,'Campaign'])
