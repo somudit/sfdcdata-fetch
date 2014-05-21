7b00cffab4e5fc022cd5e7a6db54dfd39f900991
mudits1990 <mudits1990@gmail.com>
2014-05-21 13:23:25 +0530

    mongo adapter

--- mongoadapter.py
+++ mongoadapter.py
@@ -1,12 +1,14 @@
 __author__ = 'mudit'
 from pymongo import MongoClient
 import datetime
+import json
 
 class adapter():
-
-    def push(self, collection_name):
+    def createColletion(self, collection_name):
         client = MongoClient()
         db = client.sfdc_database
-        name = collection_name
-        collection = db[name]
+        collection = db[collection_name]
+        return collection
+    def insert_posts(self, collection_name, post):
+        return collection_name.insert(post)
 
